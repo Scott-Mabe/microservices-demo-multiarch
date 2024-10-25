8b5b8afc908dc8b05967bf2967e50c9b1802ad13
Scott Mabe <90632698+Scott-Mabe@users.noreply.github.com>
2024-10-25 08:17:58 -0400

    Update quote.go

--- src/shippingservice/quote.go
+++ src/shippingservice/quote.go
@@ -1,45 +1,45 @@
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
 	"fmt"
 	"math"
-
-	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
 )
 
-// Quote represents a price with dollars and cents.
+// Quote represents a currency value.
 type Quote struct {
 	Dollars uint32
 	Cents   uint32
 }
 
-// CreateQuoteFromCount takes a number of items and returns a Quote struct.
+// String representation of the Quote.
+func (q Quote) String() string {
+	return fmt.Sprintf("$%d.%d", q.Dollars, q.Cents)
+}
+
+// CreateQuoteFromCount takes a number of items and returns a Price struct.
 func CreateQuoteFromCount(count int) Quote {
 	return CreateQuoteFromFloat(8.99)
 }
 
-// CreateQuoteFromFloat takes a price represented as a float and creates a Quote struct.
+// CreateQuoteFromFloat takes a price represented as a float and creates a Price struct.
 func CreateQuoteFromFloat(value float64) Quote {
-	span := tracer.StartSpan("quote.create")
-	defer span.Finish()
-
 	units, fraction := math.Modf(value)
-	quote := Quote{
+	return Quote{
 		uint32(units),
 		uint32(math.Trunc(fraction * 100)),
 	}
-
-	span.SetTag("quote.dollars", quote.Dollars)
-	span.SetTag("quote.cents", quote.Cents)
-	span.SetTag("quote.total", fmt.Sprintf("%d.%02d", quote.Dollars, quote.Cents))
-
-	return quote
-}
-
-func main() {
-	tracer.Start()
-	defer tracer.Stop()
-
-	quote := CreateQuoteFromCount(5)
-	fmt.Printf("Quote: %d dollars and %d cents\n", quote.Dollars, quote.Cents)
 }
